eee0c36431b3a041110aa23da7ea5f51ee7aec3e
Huchan1124 <garlic1124@gmail.com>
2021-06-25 23:15:57 +0800

    修正index.js component import 寫法

--- src/router/index.js
+++ src/router/index.js
@@ -3,37 +3,37 @@ import { createRouter, createWebHashHistory } from 'vue-router';
 const routes = [
   {
     path: '/',
-    component: import('../views/Front.vue'),
+    component: () => import('../views/Front.vue'),
     children: [
       {
         path: '',
-        component: import('../views/Index.vue')
+        component: () => import('../views/Index.vue')
       },
       {
         path: 'products',
-        component: import('../views/Products.vue')
+        component: () => import('../views/Products.vue')
       },
       {
         path: 'cart',
-        component: import('../views/Cart.vue')
+        component: () => import('../views/Cart.vue')
       }]
   },
   {
     path: '/admin',
-    component: import('../views/Dashborad.vue'),
+    component: () => import('../views/Dashborad.vue'),
     children: [{
       path: 'products',
-      component: import('../views/admin/Products.vue')
+      component: () => import('../views/admin/Products.vue')
     },
     {
       path: 'order',
-      component: import('../views/admin/Order.vue')
+      component: () => import('../views/admin/Order.vue')
     }
     ]
   },
   {
     path: '/login',
-    component: import('../views/Login.vue')
+    component: () => import('../views/Login.vue')
   }
 ];
 const router = createRouter({
